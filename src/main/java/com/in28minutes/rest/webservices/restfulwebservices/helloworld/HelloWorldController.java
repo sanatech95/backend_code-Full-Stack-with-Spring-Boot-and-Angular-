17f5684da5a32f6e7555195f96ef05acb69a7c65
sanatech95 <tasneem786786.khan@gmail.com>
2021-09-07 13:02:49 +0530

    connection spring security and spring boot

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
+
 public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/resource/JwtTokenRequest.java
@@ -0,0 +1,41 @@
+package com.in28minutes.rest.webservices.restfulwebservices.jwt.resource;
+
+import java.io.Serializable;
+
+public class  JwtTokenRequest implements Serializable {
+  
+  private static final long serialVersionUID = -5616176897013108345L;
+
+  private String username;
+    private String password;
+
+//    {
+//    	"token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbjI4bWludXRlcyIsImV4cCI6MTYzMTUyMDE3OCwiaWF0IjoxNjMwOTE1Mzc4fQ.INnFb7OR45FosEWglBsh68DN6-cw0c3LMxTAclc_hMiorXvORKivINe1yfEE5xt9SebakLKYRmmhzGga6Xe9jQ"
+//    	}
+
+    public JwtTokenRequest() {
+        super();
+    }
+
+    public JwtTokenRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
